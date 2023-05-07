bce9d709acdc93d793a0218d9d60adf06d87c9f4
B!B404 <hiherebib404@gamil.com>
2023-05-07 15:50:53 +0600

    2nd commit

---
+++ mytest.dart
@@ -0,0 +1,33 @@
+
+///Asslamualaikum here is my code, I am using user input & if else statement to solve the problem^^
+import 'dart:io';
+main(){
+  String studnetName='Bashir';
+  String studentGrade = stdin.readLineSync()!;
+  int testScore = int.parse(studentGrade);
+  if(testScore>=90 && testScore<=100)
+    {
+      print('$studnetName Grade is:A');
+    }
+  else if(testScore>=80 && testScore<=89)
+  {
+    print('$studnetName Grade is:B');
+  }
+  else if(testScore>=70 && testScore<=79)
+  {
+    print('$studnetName Grade is:C');
+  }
+  else if(testScore>=60 && testScore<=69)
+  {
+    print('$studnetName Grade is:D');
+  }
+  else if(testScore>=0 && testScore<=59)
+  {
+    print('$studnetName Grade is:F');
+  }
+  else
+  {
+    print('$studnetName Grade is:Invalid');
+  }
+}
+///Thank you^^May allah bless you AMEEN.
